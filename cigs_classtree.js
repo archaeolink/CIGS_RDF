e0c99ad08cab9dc1ddfbfde12089ccec1eec2dd7
situx <situx@users.noreply.github.com>
2023-05-12 17:09:41 +0000

    Deploying to gh-pages from @ archaeolink/CIGS_RDF@0ee0f3bde193deb22bc360e819794d43f462a118 🚀

--- cigs_classtree.js
+++ cigs_classtree.js
@@ -4121,12 +4121,6 @@ var tree={
             "http://www.w3.org/2000/01/rdf-schema#label": {
               "instancecount": 2
             },
-            "http://www.w3.org/2002/07/owl#sameAs": {
-              "instancecount": 1
-            },
-            "http://www.wikidata.org/prop/direct/P1566": {
-              "instancecount": 1
-            },
             "http://www.wikidata.org/prop/direct/P1584": {
               "instancecount": 1
             }
@@ -4138,7 +4132,7 @@ var tree={
             }
           }
         },
-        "instancecount": 10
+        "instancecount": 8
       },
       {
         "id": "http://www.opengis.net/ont/geosparql#Feature",
