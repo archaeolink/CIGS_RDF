fbef355cc218fe7bf87dc929abaf1e0c212e4a57
situx <situx@users.noreply.github.com>
2023-11-07 23:31:42 +0000

    Deploying to gh-pages from @ archaeolink/CIGS_RDF@20b3ff2a6883e5e808aa6c010885aaf649ee6da8 🚀

--- minivowl_result.js
+++ minivowl_result.js
@@ -49,10 +49,10 @@ var minivowlresult={
  "links": [
   {
    "source": 0,
-   "target": 3,
-   "valueTo": "hasGeometry",
+   "target": 7,
+   "valueTo": "partOf",
    "propertyTo": "class",
-   "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
+   "uriTo": "http://purl.org/dc/terms/partOf"
   },
   {
    "source": 0,
@@ -62,11 +62,11 @@ var minivowlresult={
    "uriTo": "http://purl.org/dc/terms/partOf"
   },
   {
-   "source": 0,
-   "target": 7,
-   "valueTo": "partOf",
+   "source": 7,
+   "target": 4,
+   "valueTo": "creator",
    "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/partOf"
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
   },
   {
    "source": 6,
@@ -76,11 +76,11 @@ var minivowlresult={
    "uriTo": "http://purl.org/dc/elements/1.1/creator"
   },
   {
-   "source": 7,
-   "target": 4,
-   "valueTo": "creator",
+   "source": 0,
+   "target": 3,
+   "valueTo": "hasGeometry",
    "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+   "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
   }
  ]
 }
