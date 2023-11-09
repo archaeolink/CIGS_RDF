5b2e9681614b8b860786d14c5a78609252e13fd0
situx <situx@users.noreply.github.com>
2023-11-09 20:36:32 +0000

    Deploying to gh-pages from @ archaeolink/CIGS_RDF@291fe6fcbac8168a689b1928fd4ab9ca9154caef 🚀

--- cigs_classtree.js
+++ cigs_classtree.js
@@ -4129,7 +4129,7 @@ var tree={
             "http://www.w3.org/2002/07/owl#sameAs": {
               "instancecount": 1
             },
-            "http://www.w3.org/2004/02/skos/core#altLabel": {
+            "http://www.wikidata.org/prop/direct/P1566": {
               "instancecount": 1
             }
           },
