c06708429441f87b425fed0817ff011f5eeb9a77
situx <situx@users.noreply.github.com>
2023-11-29 15:17:58 +0000

    Deploying to gh-pages from @ archaeolink/CIGS_RDF@a453adaa2d24af975e066505e21d83793629361e 🚀

--- cigs_classtree.js
+++ cigs_classtree.js
@@ -4125,6 +4125,18 @@ var tree={
             },
             "http://www.w3.org/2000/01/rdf-schema#label": {
               "instancecount": 2
+            },
+            "http://www.w3.org/2002/07/owl#sameAs": {
+              "instancecount": 1
+            },
+            "http://www.w3.org/2004/02/skos/core#altLabel": {
+              "instancecount": 1
+            },
+            "http://www.wikidata.org/prop/direct/P1566": {
+              "instancecount": 1
+            },
+            "http://www.wikidata.org/prop/direct/P1584": {
+              "instancecount": 1
             }
           },
           "from": {
@@ -4134,7 +4146,7 @@ var tree={
             }
           }
         },
-        "instancecount": 8
+        "instancecount": 12
       },
       {
         "id": "http://www.opengis.net/ont/geosparql#Feature",
@@ -8470,14 +8482,13 @@ var tree={
               "instancecount": 1
             },
             "http://www.w3.org/2000/01/rdf-schema#member": {
-              "instancecount": 1,
-              "http://purl.org/ontology/bibo/Document": 1,
-              "http://www.w3.org/ns/dcat#Dataset": 1
+              "instancecount": 6,
+              "http://xmlns.com/foaf/0.1/Person": 6
             }
           },
           "from": {}
         },
-        "instancecount": 3
+        "instancecount": 8
       },
       {
         "id": "http://www.opengis.net/ont/geosparql#SpatialObject",
