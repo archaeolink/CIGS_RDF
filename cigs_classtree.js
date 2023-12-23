46f588ac6c219070021e91f524b718f557525cbb
situx <situx@users.noreply.github.com>
2023-12-23 02:06:20 +0000

    Deploying to gh-pages from @ archaeolink/CIGS_RDF@b7a9a952444c5b2c87c02817c82e351c56c90d04 🚀

--- cigs_classtree.js
+++ cigs_classtree.js
@@ -4124,10 +4124,7 @@ var tree={
               "instancecount": 1
             },
             "http://www.w3.org/2000/01/rdf-schema#label": {
-              "instancecount": 1
-            },
-            "http://www.wikidata.org/prop/direct/P1566": {
-              "instancecount": 1
+              "instancecount": 3
             }
           },
           "from": {
@@ -4137,13 +4134,13 @@ var tree={
             }
           }
         },
-        "instancecount": 8
+        "instancecount": 9
       },
       {
         "id": "http://www.opengis.net/ont/geosparql#Feature",
         "parent": "#",
         "type": "class",
-        "text": "Feature (gsp:Feature)",
+        "text": "Feature (ns1:Feature)",
         "data": {}
       },
       {
@@ -8203,7 +8200,7 @@ var tree={
         "id": "http://www.opengis.net/ont/sf#Point",
         "parent": "http://www.opengis.net/ont/geosparql#Geometry",
         "type": "halfgeoclass",
-        "text": "Point (sf:Point) [579]",
+        "text": "Point [579]",
         "data": {
           "to": {
             "http://www.opengis.net/ont/geosparql#asWKT": {
@@ -8233,7 +8230,7 @@ var tree={
         "id": "http://www.opengis.net/ont/geosparql#Geometry",
         "parent": "#",
         "type": "class",
-        "text": "Geometry (gsp:Geometry)",
+        "text": "Geometry (ns1:Geometry)",
         "data": {}
       },
       {
@@ -8282,7 +8279,7 @@ var tree={
         "id": "http://xmlns.com/foaf/0.1/Person",
         "parent": "#",
         "type": "class",
-        "text": "Person (foaf:Person) [6]",
+        "text": "Person (ns7:Person) [6]",
         "data": {
           "to": {
             "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": {
@@ -8323,7 +8320,7 @@ var tree={
         "id": "http://purl.org/ontology/bibo/Document",
         "parent": "#",
         "type": "class",
-        "text": "Document (bibo:Document) [1]",
+        "text": "Document (ns8:Document) [1]",
         "data": {}
       },
       {
@@ -8384,7 +8381,7 @@ var tree={
         "id": "http://www.opengis.net/ont/geosparql#FeatureCollection",
         "parent": "http://www.opengis.net/ont/geosparql#SpatialObjectCollection",
         "type": "collectionclass",
-        "text": "FeatureCollection (gsp:FeatureCollection) [1]",
+        "text": "FeatureCollection (ns1:FeatureCollection) [1]",
         "data": {
           "to": {
             "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": {
@@ -8406,7 +8403,7 @@ var tree={
         "id": "http://www.opengis.net/ont/geosparql#SpatialObjectCollection",
         "parent": "#",
         "type": "collectionclass",
-        "text": "SpatialObjectCollection (gsp:SpatialObjectCollection)",
+        "text": "SpatialObjectCollection (ns1:SpatialObjectCollection)",
         "data": {}
       },
       {
@@ -8441,7 +8438,7 @@ var tree={
         "id": "http://www.w3.org/2004/02/skos/core#Collection",
         "parent": "#",
         "type": "collectionclass",
-        "text": "Collection (skos:Collection) [4]",
+        "text": "Collection (ns6:Collection) [4]",
         "data": {
           "to": {
             "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": {
@@ -8451,20 +8448,19 @@ var tree={
               "instancecount": 1
             },
             "http://www.w3.org/2000/01/rdf-schema#member": {
-              "instancecount": 1,
-              "http://purl.org/ontology/bibo/Document": 1,
-              "http://www.w3.org/ns/dcat#Dataset": 1
+              "instancecount": 6,
+              "http://xmlns.com/foaf/0.1/Person": 6
             }
           },
           "from": {}
         },
-        "instancecount": 3
+        "instancecount": 8
       },
       {
         "id": "http://www.opengis.net/ont/geosparql#SpatialObject",
         "parent": "#",
         "type": "class",
-        "text": "SpatialObject (gsp:SpatialObject)",
+        "text": "SpatialObject (ns1:SpatialObject)",
         "data": {}
       }
     ]
