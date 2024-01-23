06605ce48e1d223b1249a27961986cfa5663228c
situx <situx@users.noreply.github.com>
2024-01-23 14:57:42 +0000

    Deploying to gh-pages from @ archaeolink/CIGS_RDF@c1ef68a3fa58e09e66c67824f23fd7f2b3347e73 🚀

--- cigs_classtree.js
+++ cigs_classtree.js
@@ -4129,6 +4129,12 @@ var tree={
             },
             "http://www.w3.org/2000/01/rdf-schema#label": {
               "instancecount": 2
+            },
+            "http://www.w3.org/2002/07/owl#sameAs": {
+              "instancecount": 1
+            },
+            "http://www.w3.org/2004/02/skos/core#altLabel": {
+              "instancecount": 1
             }
           },
           "from": {
@@ -4138,7 +4144,7 @@ var tree={
             }
           }
         },
-        "instancecount": 9
+        "instancecount": 11
       },
       {
         "id": "http://data.archaeology.link/data/cigs/nonns_Feature.html",
