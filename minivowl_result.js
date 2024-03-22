322d02ea3632243e8050ee6aad011a28446a184b
situx <situx@users.noreply.github.com>
2024-03-22 00:42:15 +0000

    Deploying to gh-pages from @ archaeolink/CIGS_RDF@ffccb8582d5481756d74413f8f13e7d5753da22b 🚀

--- minivowl_result.js
+++ minivowl_result.js
@@ -49,14 +49,14 @@ var minivowlresult={
  "links": [
   {
    "source": 0,
-   "target": 7,
+   "target": 6,
    "valueTo": "partOf",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/terms/partOf"
   },
   {
    "source": 0,
-   "target": 6,
+   "target": 7,
    "valueTo": "partOf",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/terms/partOf"
@@ -69,14 +69,14 @@ var minivowlresult={
    "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
   },
   {
-   "source": 7,
+   "source": 6,
    "target": 4,
    "valueTo": "creator",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/elements/1.1/creator"
   },
   {
-   "source": 6,
+   "source": 7,
    "target": 4,
    "valueTo": "creator",
    "propertyTo": "class",
